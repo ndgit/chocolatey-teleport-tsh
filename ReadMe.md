27c3cf8577214d6737736845a7b864cbc956f676
Robert Rettig <robert.rettig@ndgit.com>
2021-09-15 17:03:32 +0200

    first commit
    for windows teleport tsh client version 7.1.2

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+﻿# teleport tsh client for windows
+
+Access Computing Resources Anywhere
+
+Teleport allows engineers and security professionals to unify access
+for SSH servers, Kubernetes clusters, web applications, and databases
+across all environments.
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://get.gravitational.com/teleport-v7.1.2-windows-amd64-bin.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url64bit      = $url64
+  softwareName  = 'teleport-tsh*'
+  checksum64    = '780ff7ba1da41d69908b27b67e1d02b6f2d3ea8bef3de00c23613f6296fb6d99'
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
