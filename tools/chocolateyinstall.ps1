7a835fa250d6385536eaa5117370260b1fee2326
Robert Rettig <robert.rettig@ndgit.com>
2021-10-14 19:36:22 +0200

    update 7.3.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://get.gravitational.com/teleport-v7.2.1-windows-amd64-bin.zip'
+$url64      = 'https://get.gravitational.com/teleport-v7.3.0-windows-amd64-bin.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url64bit      = $url64
   softwareName  = 'teleport-tsh*'
-  checksum64    = 'ef8dbb85cf31327e36ab70b6c4737b18f0ebc4ecd6a9699b2222ab1b41729406'
+  checksum64    = 'c4b90442eb6328d9d85cee51aff361fc5bd7116742108ddaa2733f4ca7792036'
   checksumType64= 'sha256'
 }
 
