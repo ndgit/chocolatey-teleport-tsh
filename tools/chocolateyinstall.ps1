910e8145e26060bf51d3dd31f80bb61f678b7d40
Robert Rettig <robert.rettig@ndgit.com>
2022-05-23 16:49:29 +0200

    update teleport-tsh 9.1.3

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://get.gravitational.com/teleport-v9.1.2-windows-amd64-bin.zip'
+$url64      = 'https://get.gravitational.com/teleport-v9.1.3-windows-amd64-bin.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url64bit      = $url64
   softwareName  = 'teleport-tsh*'
-  checksum64    = '4ac1706a488abd420f49c7a7450818da68d74ba753d5f5d5bc6053dd3a796a3f'
+  checksum64    = '5af7b970ca18b39567d41e704bd2d5c0b6ef2683989c98520c108206357b407a'
   checksumType64= 'sha256'
 }
 
