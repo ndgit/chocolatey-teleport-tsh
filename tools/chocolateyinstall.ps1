a13c836200fa65adf9f8363098a282fb8cb5e589
Robert Rettig <robert.rettig@ndgit.com>
2023-01-19 09:22:29 +0100

    update teleport-tsh 11.2.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,15 +1,21 @@
 ﻿
 $ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://cdn.teleport.dev/teleport-v11.0.3-windows-amd64-bin.zip'
-
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  url64bit      = $url64
-  softwareName  = 'teleport-tsh*'
-  checksum64    = '7e463f224a1bdeb1f737276216b6a87dd9c786ab6f35d8a061e1666d954d0202'
-  checksumType64= 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64 = 'https://cdn.teleport.dev/teleport-v11.2.2-windows-amd64-bin.zip'
+$options = 
+@{
+  Headers = @{
+    Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
+  }
 }
 
-Install-ChocolateyZipPackage @packageArgs
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  url64bit       = $url64
+  softwareName   = 'teleport-tsh*'
+  checksum64     = '4d0691e7c52250e98f8592456cf51f017c2904f83ddc786beac9d60e61e96759'
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs -Options $options
